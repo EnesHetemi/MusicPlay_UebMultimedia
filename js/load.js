080fadb8b3509398bb7e28fedd3d243cc6ef798f
EnesHetemi <eneshetemi3@gmail.com>
2024-06-06 13:28:56 +0200

    Load dhe audio

---
+++ js/load.js
@@ -0,0 +1,7 @@
+const loader = document.getElementById('loader');
+
+window.addEventListener('load', function () {
+    setTimeout(function () {
+    loader.style.display = 'none';
+    }, 2000);
+});
